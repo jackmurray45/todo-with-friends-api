aa5c07a3ed67d94b388dec69e9a1f78fa3f09282
Jack Murray <jackmurray445@gmail.com>
2020-09-07 11:58:34 -0700

    setup for todo_friends model tests

--- test/models/todo_friend_test.rb
+++ test/models/todo_friend_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
+require 'faker'
 
 class TodoFriendTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.create(name: Faker::Name.name, email: Faker::Internet.email,
+                     password: "foobar", password_confirmation: "foobar")
+
+    todo = @user.todos.new(title: Faker::Lorem.word, due_by: Faker::Time)
+  end
+
+  test "can only add friends to todo" do
+    assert false
+  end
+
+  test "cannot add user twice" do
+    assert false
+  end
+
+  def 
 end
