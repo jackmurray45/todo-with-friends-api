089b32f44140e08d1e8fb3351b174f1612e5f0dd
Jack Murray <jackmurray445@gmail.com>
2020-09-07 17:25:16 -0700

    filled out todo_friend_test

--- test/models/todo_friend_test.rb
+++ test/models/todo_friend_test.rb
@@ -7,15 +7,53 @@ class TodoFriendTest < ActiveSupport::TestCase
     @user = User.create(name: Faker::Name.name, email: Faker::Internet.email,
                      password: "foobar", password_confirmation: "foobar")
 
-    todo = @user.todos.new(title: Faker::Lorem.word)
+    @todo = @user.todos.create(title: Faker::Lorem.word)
+
   end
 
   test "can only add friends to todo" do
-    assert false
+
+    #create new user and make them friends with first user
+    new_user =  User.create(name: Faker::Name.name, email: Faker::Internet.email,
+      password: "foobar", password_confirmation: "foobar")
+
+    @user.friendships.create(friend_id: new_user.id)
+    new_user.friendships.create(friend_id: @user.id)
+
+    todo_friend = @todo.todo_friends.new(user_id: new_user.id)
+
+    assert todo_friend.valid?
+
+    #create second user that is not friends and try to add them to the Todo
+    second_new_user = User.create(name: Faker::Name.name, email: Faker::Internet.email,
+      password: "foobar", password_confirmation: "foobar")
+
+    todo_friend_fail = @todo.todo_friends.new(user_id: second_new_user)
+    assert_not todo_friend_fail.valid?
+
+    
   end
 
   test "cannot add user twice" do
-    assert false
+    #create new user and make them friends with first user
+    new_user =  User.create(name: Faker::Name.name, email: Faker::Internet.email,
+      password: "foobar", password_confirmation: "foobar")
+
+    @user.friendships.create(friend_id: new_user.id)
+    new_user.friendships.create(friend_id: @user.id)
+
+    @todo.todo_friends.create(user_id: new_user.id)
+
+    todo_friend_fail = @todo.todo_friends.new(user_id: new_user.id)
+
+    assert_not todo_friend_fail.valid?
+  end
+
+  test "todo owner cannot be todo friend" do
+
+    todo_friend_fail = @todo.todo_friends.create(user_id: @user.id)
+
+    assert_not todo_friend_fail.valid?
   end
 
 end
